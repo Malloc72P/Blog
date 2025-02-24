bcef0691013c0d8e996f5fb580db8b89b56bebb6
Malloc72P <scra1028@gmail.com>
2025-02-24 23:42:15 +0900

    Chore: 누락된 주석 추가

--- apps/blog/libs/get-posts.ts
+++ apps/blog/libs/get-posts.ts
@@ -13,7 +13,7 @@ export async function getPosts() {
    * normalizePages는 각 페이지의 구조와 메타데이터를 기반으로 계층적이고 일관된 네비게이션 데이터를 생성하는 유틸리티이다.
    * getPageMap에서 라우트 경로를 적어주면 반환 타입이 PageMapItem인데, 이 경우 타입이 Folder | MdxFile | MetaJsonFile이다.
    * 이러한 페이지 맵을 정규화하여, 폴더 목록, 경로, 메타데이터 등을 제공한다.
-   * mdx 페이지는 directories에 섞여있으니,
+   * mdx 페이지는 directories에서 찾을 수 있다.
    */
   const { directories } = normalizePages({
     list: pageMap,
