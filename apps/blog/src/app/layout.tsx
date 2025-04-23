1f9051a877a7ef11bc815af5e04cd109ab2495e6
Malloc72P <scra1028@gmail.com>
2025-04-23 21:12:04 +0900

    Chore: 파비콘 용량 축소

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko" className={`${font.variable}`}>
       <head>
-        <link rel="icon" type="image/png" href="/favicon.png"></link>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
 
