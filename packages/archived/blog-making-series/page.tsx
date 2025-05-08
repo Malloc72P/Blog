98fa0aa051755a7283884bf5deb2d7d3d1c36e28
Malloc72P <scra1028@gmail.com>
2025-05-08 23:23:01 +0900

    Post: 다시 처음부터 작성하기로 함
     - 글이 너무너무 마음에 안들어서 기존 글을 아카이빙하고 새로 작성시작함.
     - 일단 이전 글 보다는 훨 나은 것 같다. 솔직히 뭔 소리 하는지 글을 쓴 나도 이해가 안갔음

---
+++ packages/archived/blog-making-series/page.tsx
@@ -0,0 +1,13 @@
+import { SeriesDetail } from '@components/series-detail/series-detail';
+import { DateUtil } from '@libs/date-util';
+
+export default async function BlogMakingSeriesPage() {
+  return <SeriesDetail series={{ ...metadata }} />;
+}
+
+export const metadata = {
+  id: 'blog-making-series',
+  title: '블로그 만들기 프로젝트',
+  date: '2025-03-08 21:18',
+  isSeriesLanding: true,
+};
